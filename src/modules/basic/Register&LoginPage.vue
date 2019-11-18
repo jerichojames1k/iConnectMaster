ed4ea7bdc07353d92abfd420d3067e39f241b8d6
2ndyrGroupB <2ndyrGroupB@pn.local>
2019-11-18 19:43:29 +0800

    MODAL WAS MODIFIED

--- src/modules/basic/Register&LoginPage.vue
+++ src/modules/basic/Register&LoginPage.vue
@@ -59,48 +59,69 @@
                 type="password"
               ></b-form-input>
             </b-form-group>
-
-            <center>
-              <p>
-                By clicking Sign Up, you agree to our
-                <a
-                  href="#"
-                  v-b-modal.modal-scrollable
-                >Terms & Conditions</a>
-              </p>
-              <b-modal
-                id="modal-scrollable"
-                scrollable
-                title="Our Terms & Conditions"
-                ok-only
-                no-stacking
-                @ok="handleOk"
-              >
-                <p class="my-4" @submit.stop.prevent="handleSubmit">
-                  Sample Terms and Conditions for Website
-                  Your Nowhere Town Terms of Service ("Agreement")
-                  This Agreement was last modified on September 20, 2013.
-                  Please read these Terms of Service completely using yournowheretownblog.com which is owned and operated by Your Nowhere Town. This Agreement documents the legally binding terms and conditions attached to the use of the Site at yournowheretownblog.com.
-                  By using or accessing the Site in any way, viewing or browsing the Site, or adding your own content to the Site, you are agreeing to be bound by these Terms of Service.
-                  Intellectual Property
-                  The Site and all of its original content are the sole property of Your Nowhere Town and are, as such, fully protected by the appropriate international copyright and other intellectual property rights laws.
-                  Termination
-                  Your Nowhere Town reserves the right to terminate your access to the Site, without any advance notice.
-                  Links to Other Websites
-                  Our Site does contain a number of links to other websites and online resources that are not owned or controlled by Your Nowhere Town.
-                  Your Nowhere Town has no control over, and therefore cannot assume responsibility for, the content or general practices of any of these third party sites and/or services. Therefore, we strongly advise you to read the entire terms and conditions and privacy policy of any site that you visit as a result of following a link that is posted on our site.
-                  Governing Law
-                  This Agreement is governed in accordance with the laws of New Jersey, United States.
-                  Changes to This Agreement
-                  Your Nowhere Town reserves the right to modify these Terms of Service at any time. We do so by posting and drawing attention to the updated terms on the Site. Your decision to continue to visit and make use of the Site after such changes have been made constitutes your formal acceptance of the new Terms of Service.
-                  Therefore, we ask that you check and review this Agreement for such changes on an occasional basis. Should you not agree to any provision of this Agreement or any changes we make to this Agreement, we ask and advise that you do not use or continue to access the Your Nowhere Town site immediately.
-                  Contact Us
-                  If you have any questions about this Agreement, please feel free to contact us at info@yournowheretown.com.
-                </p>
-              </b-modal>
-     
-              <b-button id="login" type="submit">Sign Up</b-button>
-            </center>
+           <center>
+      <p>
+        By clicking Sign Up, you agree to our
+        <a
+          href="#"
+          v-b-modal.modal-scrollable
+          @click="show=true"
+        >Terms & Conditions</a>
+      </p>
+      <!-- <b-modal
+        id="modal-scrollable"
+        scrollable
+        title="Our Terms & Conditions"
+        ok-only
+        no-stacking
+        @ok="handleOk"
+      >
+      </b-modal> -->
+        <div>
+    
+    <b-modal   
+       scrollable title="Our Terms and Conditions"
+       id="modal-scrollable" 
+       v-model="show">
+            <p>
+          Iconnect Terms of Service ("Agreement")
+          This Agreement was last modified on September 20, 2013.
+          Please read these Terms of Service completely using iconnectblog.com which is owned and operated by Iconnect. This Agreement documents the legally binding terms and conditions attached to the use of the Site at iconnectblog.com.
+          By using or accessing the Site in any way, viewing or browsing the Site, or adding your own content to the Site, you are agreeing to be bound by these Terms of Service.
+          </p>
+          <ul>
+            <li> Intellectual Property</li>
+            <p>The Site and all of its original content are the sole property of Iconnect and are, as such, fully protected by the appropriate international copyright and other intellectual property rights laws.
+            </p>
+            <li> Termination</li>
+            <p>Iconnect reserves the right to terminate your access to the Site, without any advance notice.Links to Other Websites
+          Our Site does contain a number of links to other websites and online resources that are not owned or controlled by Iconnect.
+          Iconnect has no control over, and therefore cannot assume responsibility for, the content or general practices of any of these third party sites and/or services. Therefore, we strongly advise you to read the entire terms and conditions and privacy policy of any site that you visit as a result of following a link that is posted on our site.</p>
+            <li>Governing Law</li>
+            <p>This Agreement is governed in accordance with the laws of Philippines.</p>
+            <li>Chages to This Agreement</li>
+            <p>Iconnect reserves the right to modify these Terms of Service at any time. We do so by posting and drawing attention to the updated terms on the Site. Your decision to continue to visit and make use of the Site after such changes have been made constitutes your formal acceptance of the new Terms of Service.
+          Therefore, we ask that you check and review this Agreement for such changes on an occasional basis. Should you not agree to any provision of this Agreement or any changes we make to this Agreement, we ask and advise that you do not use or continue to access the Iconnect site immediately.</p>
+            <li>Contact Us</li>
+            <p>   If you have any questions about this Agreement, please feel free to contact us at info@iconnect.com.</p>
+          </ul>
+      <template v-slot:modal-footer>
+        <div class="w-100">
+          <b-button
+            id="modal"
+            size="sm"
+            class="float-right"
+            @click="show=false"
+            type="dark"
+          >
+            Close
+          </b-button>
+        </div>
+      </template>
+    </b-modal>
+  </div>
+      <b-button id="login" type="submit">Sign Up</b-button>
+    </center>
             <br />
           </b-form>
         </b-col>
@@ -122,7 +143,7 @@ export default {
         password: "",
         confirmPassword: ""
       },
-      show: true
+      show:false
     };
   },
   methods: {
@@ -162,6 +183,9 @@ export default {
 
 <style scoped lang="scss">
 @import "~assets/color.scss";
+#modal{
+  background-color:#BB6BD9;
+}
 #top {
   background-color: white;
   padding: 2vw 3vw 4vw;
